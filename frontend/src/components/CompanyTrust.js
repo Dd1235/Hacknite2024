e5246485b56b7bddcf30e3e8076ea88ab48702fc
Dedeepya Avancha <avancha.dedeepya@iiitb.ac.in>
2024-04-01 00:50:50 +0530

    slider

--- frontend/src/components/CompanyTrust.js
+++ frontend/src/components/CompanyTrust.js
@@ -1,41 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import logo1 from "./logo-1.webp";
 import logo2 from "./logo-2.webp";
 import logo3 from "./logo-3.webp";
 import logo4 from "./logo-4.svg";
 import logo5 from "./logo-5.webp";
 import logo6 from "./logo-6.png";
+import "./CompanyTrust.css"; // Import CSS file for animation styles
+import Slider from "react-infinite-logo-slider";
 
-export default function CompanyTrust() {
+const Component = () => {
   return (
     <div className="flex flex-col items-center justify-center py-14">
-      <div className="playfair font-bold text-center w-4/5 black-text dark:white-text  text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+      <div className="playfair font-bold text-center w-4/5 black-text dark:white-text  text-3xl sm:text-4xl md:text-5xl lg:text-6xl mb-10">
         Trusted by over 20,000 brands
       </div>
-      <div className="flex flex-col items-center justify-center w-3/5 gap-5 sm:gap-10 mt-10 md:mt-20">
-        <div className="flex flex-row gap-5 sm:gap-10">
-          <div className="white-bg rounded-md transform-scale h-16 w-16 sm:h-32 sm:w-32 overflow-hidden flex justify-center items-center">
-            <img src={logo1} className="h-14 sm:h-28 sm:w-24" />
-          </div>
-          <div className="white-bg rounded-md transform-scale h-16 w-16 sm:h-32 sm:w-32 overflow-hidden flex justify-center items-center">
-            <img src={logo2} className="h-14 sm:h-28 sm:w-52 max-w-none" />
-          </div>
-          <div className="white-bg rounded-md transform-scale h-16 w-16 sm:h-32 sm:w-32 overflow-hidden flex justify-center items-center">
-            <img src={logo3} className="w-14 sm:w-32 sm:h-8" />
-          </div>
-        </div>
-        <div className="flex flex-row gap-5 sm:gap-10">
-          <div className="white-bg rounded-md transform-scale h-16 w-16 sm:h-32 sm:w-32 overflow-hidden flex justify-center items-center">
-            <img src={logo4} className="w-14 sm:w-28 max-h-none" />
-          </div>
-          <div className="white-bg rounded-md transform-scale h-16 w-16 sm:h-32 sm:w-32 overflow-hidden flex justify-center items-center">
-            <img src={logo5} className="h-14 sm:h-28" />
-          </div>
-          <div className="white-bg rounded-md transform-scale h-16 w-16 sm:h-32 sm:w-32 overflow-hidden flex justify-center items-center">
-            <img src={logo6} className="h-14 sm:h-28" />
-          </div>
-        </div>
-      </div>
+      <Slider
+        width="250px"
+        duration={40}
+        pauseOnHover={true}
+        blurBorders={false}
+        blurBoderColor={"#fff"}
+      >
+        <Slider.Slide>
+          <img src={logo1} alt="logo1" className="w-36" />
+        </Slider.Slide>
+        <Slider.Slide>
+          <img src={logo2} alt="logo2" className="w-36" />
+        </Slider.Slide>
+        <Slider.Slide>
+          <img src={logo3} alt="logo3" className="w-36" />
+        </Slider.Slide>
+        <Slider.Slide>
+          <img src={logo4} alt="logo4" className="w-36" />
+        </Slider.Slide>
+        <Slider.Slide>
+          <img src={logo5} alt="logo5" className="w-36" />
+        </Slider.Slide>
+        <Slider.Slide>
+          <img src={logo6} alt="logo6" className="w-36" />
+        </Slider.Slide>
+      </Slider>
     </div>
   );
-}
+};
+
+export default Component;
