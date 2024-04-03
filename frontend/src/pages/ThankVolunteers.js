1cb3d6eac3277fda9e0bbc598d5dd0823ad46de8
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-04-03 21:52:59 +0530

    bheegi billi

--- frontend/src/pages/ThankVolunteers.js
+++ frontend/src/pages/ThankVolunteers.js
@@ -3,7 +3,16 @@ import NyanCat from "../components/NyanCat";
 
 export default function ThankVolunteers() {
   return (
-    <div>
+    <div className="h-screen">
+      <div className="black-text text-left gap-10 dark:white-text sanspro h-screen flex flex-col items-center justify-center">
+        <h1 className="playfair text-5xl w-4/5 sm:text-7xl font-bold z-10 py-2 sm:py-4">
+          Thank You!
+        </h1>
+        <p className="text-sm sm:text-lg w-4/5 z-10">
+          Your form is pending.
+          <br /> You will a recieve an email if your form is approved.
+        </p>
+      </div>
       <NyanCat />
     </div>
   );
