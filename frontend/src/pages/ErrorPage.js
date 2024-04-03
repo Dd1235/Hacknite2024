7cd1884cdd14815115f7342149b11d3cb5b8a4b4
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-04-03 13:48:06 +0530

    oops page

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,18 +1,25 @@
 import { useRouteError } from "react-router-dom";
+import ParticlesComponent from "../components/ParticlesComponent";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-5">
-      <h1 className="text-7xl playfair font-bold">Oops!</h1>
-      <p className="text-lg sanspro">
-        Sorry, an unexpected error has occurred.
-      </p>
-      <p className="text-lg">
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="h-screen w-screen">
+      <div
+        className="z-10 h-full white-text w-full flex flex-col items-center justify-center gap-5"
+        style={{ backgroundColor: "#00054c" }}
+      >
+        <h1 className="text-7xl playfair font-bold">Oops!</h1>
+        <p className="text-lg sanspro">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="text-lg">
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+      <ParticlesComponent />
     </div>
   );
 }
