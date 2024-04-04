fb1613136ca721b6c3fcc5f7d1f8d0bda252debf
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-04-04 10:34:44 +0530

    sh: line 1: q: command not found

--- frontend/src/pages/ThankVolunteers.js
+++ frontend/src/pages/ThankVolunteers.js
@@ -4,7 +4,7 @@ import NyanCat from "../components/NyanCat";
 export default function ThankVolunteers() {
   return (
     <div className="h-screen">
-      <div className="black-text text-left gap-10 dark:white-text sanspro h-screen flex flex-col items-center justify-center">
+      <div className="text-left gap-10 white-text sanspro h-screen flex flex-col items-center justify-center">
         <h1 className="playfair text-5xl w-4/5 sm:text-7xl font-bold z-10 py-2 sm:py-4">
           Thank You!
         </h1>
