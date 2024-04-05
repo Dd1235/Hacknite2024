9e5c5060cc4e06a477fda99839786ae0f645d1b7
Dedeepya Avancha <avancha.dedeepya@iiitb.ac.in>
2024-04-05 13:51:54 +0530

    organized partner code

---
+++ backend/controllers/partnerController.js
@@ -0,0 +1,53 @@
+const Partner = require("../models/partnerModel");
+
+const createPartner = async (req, res) => {
+  const { institution, firstName, lastName, phoneNumber, email, message } =
+    req.body;
+  try {
+    const newPartner = await Partner.create({
+      institution,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      message,
+      status: "pending",
+    });
+    res.status(200).json({
+      message: "Partner registered successfully.",
+      partner: newPartner,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
+
+const partnerStats = async (req, res) => {
+  try {
+    const totalPartners = await Partner.countDocuments();
+
+    const partnersPerYear = await Partner.aggregate([
+      {
+        $group: {
+          _id: { $year: "$createdAt" },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+
+    res.json({
+      totalPartners,
+      partnersPerYear,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createPartner,
+  partnerStats,
+};
--- backend/routes/partners.js
+++ backend/routes/partners.js
@@ -1,54 +1,14 @@
 const express = require("express");
 const Partner = require("../models/partnerModel");
-const mongoose = require("mongoose");
+const {
+  createPartner,
+  partnerStats,
+} = require("../controllers/partnerController");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { institution, firstName, lastName, phoneNumber, email, message } =
-    req.body;
-  try {
-    const newPartner = await Partner.create({
-      institution,
-      firstName,
-      lastName,
-      phoneNumber,
-      email,
-      message,
-      status: "pending",
-    });
-    res.status(200).json({
-      message: "Partner registered successfully.",
-      partner: newPartner,
-    });
-  } catch (error) {
-    res.status(400).json({
-      error: error.message,
-    });
-  }
-});
+router.post("/", createPartner);
 
-router.get("/stats", async (req, res) => {
-  try {
-    const totalPartners = await Partner.countDocuments();
-
-    const partnersPerYear = await Partner.aggregate([
-      {
-        $group: {
-          _id: { $year: "$createdAt" },
-          count: { $sum: 1 },
-        },
-      },
-      { $sort: { _id: 1 } },
-    ]);
-
-    res.json({
-      totalPartners,
-      partnersPerYear,
-    });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.get("/stats", partnerStats);
 
 module.exports = router;
