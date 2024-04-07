a85718aaa2e56c8151233218b31c785b86d6b6d7
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-04-07 13:40:40 +0530

    ui for forgot password

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -33,23 +33,24 @@ const Login = (props) => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        >
+        <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img
             className="w-8 h-8 mr-2"
             src={props.isDarkMode ? Logo : drkLogo}
             alt="logo"
           />
           EduReach
-        </a>
+        </div>
         <form
           className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
           onSubmit={handleSubmit}
         >
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h3 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <i
+                onClick={() => navigate(-1)}
+                className="fa-solid fa-right-from-bracket cursor-pointer mr-2 text-gray-500 transition duration-75 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+              ></i>
               Sign in to your Account
             </h3>
             <div className="space-y-4 md:space-y-6">
