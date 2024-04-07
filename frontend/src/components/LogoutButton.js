11d19018df416765cbf13afc7b029f1ef8cb0c53
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-04-07 14:39:14 +0530

    scroll

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -15,7 +15,7 @@ const LogoutButton = () => {
       className="flex w-full items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
       onClick={handleLogout}
     >
-      <i className="fa-solid fa-clock  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+      <i className="fa-solid fa-power-off  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
       <span className="ms-3">Logout</span>
     </button>
   );
--- frontend/src/pages/HandleApplications.js
+++ frontend/src/pages/HandleApplications.js
@@ -460,7 +460,10 @@ function MainPage(props) {
                     Switch to {props.isDarkMode ? "Light Mode" : "Dark Mode"}
                   </span>
                 </button>
-                <button className=" flex lg:hidden w-full justify-center items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <button
+                  onClick={() => setIsSidebar(false)}
+                  className=" flex lg:hidden w-full justify-center items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
                   <i className="fa-solid fa-right-from-bracket  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
                 </button>
               </ul>
